72f2f53d396d758c4dc878f60894862404faf6bc
tadamcz <tadamczewskipublic@gmail.com>
2023-06-18 21:33:30 +0200

    set versioned tags

--- buildx.py
+++ buildx.py
@@ -1,20 +1,28 @@
 import subprocess
 from pathlib import Path
 
-BUILDER_NAME = "buildx_builder"
+BUILDER_NAME = "nondescriptspoon_buildx_builder"
 subprocess.run(["docker", "buildx", "create", "--use", f"--name={BUILDER_NAME}"])
 
 PLATFORM = ["linux/amd64", "linux/arm64"]
 PLATFORM = ",".join(PLATFORM)
 
-IMAGES = {
-    # directory: tag
-    "pygo": "nondescriptspoon/pygo:latest"
-}
+DOCKER_HUB_USERNAME = "nondescriptspoon"
+IMAGE_DIRS = ["pygo"]
 
-for directory, tag in IMAGES.items():
-    print(f"Building image in {directory} with tag {tag}")
-    dockerfile = Path(f"{directory}/Dockerfile").read_bytes()
+
+images = {
+    # path: tag
+}
+for image_dir in IMAGE_DIRS:
+    for dockerfile in Path(image_dir).glob("*.Dockerfile"):
+        version_tag = dockerfile.stem
+        tag = f"{DOCKER_HUB_USERNAME}/{image_dir}:{version_tag}"
+        images[dockerfile] = tag
+
+for path, tag in images.items():
+    print(f"Building {tag}...")
+    dockerfile = path.read_bytes()
 
     # This will read a Dockerfile from STDIN without context. Due to the lack of a context, no contents of any local
     # directory will be sent to the Docker daemon. See https://stackoverflow.com/a/54666214/8010877
